5e055fcc0d1f99d6e02759149ad09bd8ea965ec6
Bhanujs18 <bhanupratap19995@gmail.com>
2024-01-05 23:02:59 +0530

    publish

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var position = 0;
+var resultv = 0;
+var final = 0;
 
 function reset () {
     document.getElementById('input').value = "";
+    final = 0;
 }
 
 function del() {
     document.getElementById('input').value = document.getElementById('input').value.slice(0,-1);
 }
 
-function input(input) {
-document.getElementById('input').value += input;
+function input(value) {
+document.getElementById('input').value += value;
+if(value=='result'){
+result(final)
+}
+else if(value=='reset'){
+    reset();
+}
+else{
+    final =  eval(document.getElementById('input').value )
+}
 position++;
 }
 
-function result() {
-    document.getElementById('input').value = 0;
+function result(res) {
+    document.getElementById('input').value = res;
 }
